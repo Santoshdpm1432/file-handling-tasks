dc428ff831812dd511e1e4dfe87ab13808af8e30
santosh kumar agepogu <santoshkumaragepogu484@gmail.com>
2025-03-17 01:02:56 -0600

    Add files via upload

---
+++ file_handling.py
@@ -0,0 +1,115 @@
+# mode = 'r' ---> read operations
+
+# file = open("santosh.txt",mode = 'w+')
+# file.close()
+
+# file = open("santosh.txt",mode = 'r+')
+# file.seek(0)
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# file = open("santosh.txt", mode= 'a+')
+# # write_data= file.writelines(["jitendra"])
+# file.seek(0)
+# read_data= file.read()
+# print(read_data)
+# file.close()
+
+# file = open("siri.txt", mode= 'a+')
+# file.seek(0)
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# file = open("siri.txt", mode= 'a+')
+# read_data = file.readline("santosh is the python devaloer ")
+# print(read_data)
+# file.clo
+
+# mode = 'r'
+
+# file = open("santosh.txt",mode = 'r')
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+
+# file = open("santosh.txt",mode = 'r')
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# file = open("santosh.txt",mode = 'r')
+# read_data = file.readline()
+# print(read_data)
+# file.close()
+
+# file = open("santosh.txt",mode = 'r')
+# read_data = file.readlines()
+# print(read_data)
+# file.close()
+
+# mode = 'a'
+
+# file = open("santosh.txt",mode = 'a')
+# write_data = file.write("\n santosh witing into santosh.txt by using mode a")
+# print(write_data)
+# file.close()
+
+# file = open("santosh.txt",mode = 'a')
+# write_data = file.write("\n santosh witing into santosh.txt by using mode a")
+# print(write_data)
+# file.close()
+
+# file = open("sirisha.txt",mode = 'a')
+# write_data = file.write("sirisha is a nurse in  mr hospital ")
+# print(write_data)
+# file.close()
+
+# mode = 'w'
+
+# file = open("srinivasulu.txt",mode = 'w')
+# write_data = file.write("what is going on in santosh life")
+# file.close()
+
+# file = open("srinivasulu.txt",mode = 'w')
+# write_data = file.write("lets have some cofee together")
+# file.close()
+
+
+# mode = r+
+
+# file = open("santosh.txt",mode = 'r+')
+# write_data = file.write("the above mentioned all were frineds")
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# mode = 'w+'
+
+# file = open("santosh1.txt",mode = 'w+')
+# write_date = file.write("sampletest")
+# print(file.tell())
+# file.seek(0)
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# file = open("santosh.txt",mode = 'w+')
+# write_date = file.write("sampletest")
+# write1 = file.write("\n lets have a cofee")
+# print(file.tell())
+# file.seek(0)
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# file = open("C:\\Users\\santo\OneDrive\\Documents\\random1_program.txt",mode = 'w')
+# write_data = file.write('this program should run')
+# file.close()
+
+# file = open("C:\\Users\\santo\OneDrive\\Documents\\random_program.txt",mode = 'r')
+# read_data = file.read()
+# print(read_data)
+# file.close()
